35bece8d978de721d25f299547fa9cf266b251e4
Gabriel C Moraru <gabrielcmoraru@gmail.com>
2018-09-04 14:46:39 +0100

    feat:query variables

--- src/Posts/Posts.js
+++ src/Posts/Posts.js
@@ -6,18 +6,22 @@ import { Link } from 'react-router-dom'
 export default class Posts extends Component {
   render() {
     return (
-      <Query query={POSTS_QUERY}>
-      {({loading, data}) => {
-        if (loading) return 'Loading...';
-        const { posts } = data;
-        return posts.map( post => (
-          <Link key={post.id} to={`/post/${post.id}`}>
-            <h1>{post.title}</h1>
-          </Link>
-          )
-        );
-      }}
-    </Query>
+      <ul>
+        <Query query={POSTS_QUERY}>
+        {({loading, data}) => {
+          if (loading) return 'Loading...';
+          const { posts } = data;
+          return posts.map( post => (
+            <li key={post.id}>
+              <Link to={`/post/${post.id}`}>
+                <h1>{post.title}</h1>
+              </Link>
+            </li>
+            )
+          );
+        }}
+        </Query>
+      </ul>
     )
   }
 }
